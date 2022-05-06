4635c127f01358940c76e2fa1eec333038d43e64
girumtim <girumtim@gmail.com>
2022-05-05 17:22:49 -0700

    task-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -  computes the absolute value of an integer
+ * @n: parameter for an integer
+ *
+ * Return: multiplicative inverse of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
