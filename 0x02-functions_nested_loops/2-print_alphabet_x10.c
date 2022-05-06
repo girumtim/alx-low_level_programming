0aa47ad8cd1a3727824dda21ac04f8907c8b35eb
girumtim <girumtim@gmail.com>
2022-05-05 17:09:56 -0700

    task-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_alphabet_x10 -  prints 10 times the alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 void print_alphabet_x10(void)
