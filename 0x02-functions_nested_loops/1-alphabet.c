320f177c31379bc153c3a232fcaab22aed58a389
girumtim <girumtim@gmail.com>
2022-05-05 17:07:55 -0700

    task-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_alphabet -  prints the alphabet in lowercase, followed by a new line
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 void print_alphabet(void)
