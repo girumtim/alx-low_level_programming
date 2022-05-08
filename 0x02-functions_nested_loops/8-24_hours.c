65898a96063c2d6016956c6b30419a72bbcc7525
girumtim <girumtim@gmail.com>
2022-05-08 10:31:48 -0700

    task-8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -  prints every minute of the day
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(58);
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
