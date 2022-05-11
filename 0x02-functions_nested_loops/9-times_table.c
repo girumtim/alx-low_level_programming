39a16d43ff13cd2b62a31bf134e6b4b65d6ccfb2
girumtim <girumtim@gmail.com>
2022-05-10 22:56:05 -0700

    task-9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@ void times_table(void)
 {
 	int i, j, x;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			x = i * j;
 			if (j == 0)
