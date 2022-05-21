279ec2e69f3f2ed2be8b1b6856850f29c191af9c
girumtim <girumtim@gmail.com>
2022-05-21 09:38:44 -0700

    task-1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit -  checks for a digit (0 through 9)
+ * @c: parameter for a character
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
