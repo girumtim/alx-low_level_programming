33f0e9ed6174aeda544d8bbb3e7b5764365f79b4
girumtim <girumtim@gmail.com>
2022-05-21 09:29:43 -0700

    task-0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * @c: parameter for a character
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
