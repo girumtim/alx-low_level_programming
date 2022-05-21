82502c733bd1348a7ee84f882aceb673b5a075c7
girumtim <girumtim@gmail.com>
2022-05-21 09:48:57 -0700

    task-2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two integers
+ * @a: parameter for an integer
+ * @b: parameter for an integer
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
